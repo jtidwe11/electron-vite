204acb8dbeaa2474eaa9f47a44ad62603e6e3568
Justin Tidwell <tidpickle@gmail.com>
2020-05-17 22:48:40 -0500

    add basic menu example

---
+++ app/menu.js
@@ -0,0 +1,27 @@
+const { Menu } = require('electron')
+
+const isMac = process.platform === 'darwin'
+
+const template = [
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://electronjs.org')
+        }
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
